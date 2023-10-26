c2b8ae92269e874d4ee156aec1e8cc69f5046f2c
kunal <kunaljoshi805@gmail.com>
2023-10-26 18:18:19 +0530

    my second commit

--- enroll/views.py
+++ enroll/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
-# Create your views here.
+from .models import User
 
 def add_show(request):
           if request.method == "POST":
                   fm = StudentRegistration(request.POST)
+                  if fm.is_valid():
+                          nm = fm.cleaned_data['name']
+                          em = fm.cleaned_data['email']
+                          pw = fm.cleaned_data['password']
+                          reg = User(name=nm, email=em, password=pw)
+                          reg.save()
           else:
                   fm = StudentRegistration()
           return render(request, 'enroll/addandshow.html',{'form':fm})
